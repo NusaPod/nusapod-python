04bf91928302ec0c4876b8f93bc634169d6d686c
Oji Setyawan <ojiset22@outlook.com>
2025-10-16 14:59:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, Docker Flaskx!"})
+    return jsonify({"message": "Hello, Docker Flask + CI/CD!"})
 
 @app.route('/add/<int:a>/<int:b>')
 def add(a, b):
